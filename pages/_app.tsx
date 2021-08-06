7e73599eb964afa63430bd9e4ddbd03473336d16
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2021-08-06 12:11:57 +0300

    Got rid of the navbar and about Wrote custom container styles in tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
 
-import Navbar from "components/Navbar";
-
-import "bootstrap/dist/css/bootstrap-grid.min.css";
-import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="App container">
+    <div className="font-body pb-16">
       <Head>
         <title>Noam Alffasy</title>
         <meta
@@ -16,13 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
         />
       </Head>
-      <Navbar />
       <Component {...pageProps} />
-      <style jsx>{`
-        .App {
-          padding-top: calc(0.5rem * 2 + 0.3125rem * 2 + 1.25rem + 0.5rem);
-        }
-      `}</style>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,15 @@ import * as React from "react";
 import { Component } from "react";
 
 import Top from "components/Top";
-import About from "components/About";
 import Projects from "components/Projects";
 
 class Index extends Component {
   render() {
     return (
-      <div className="Index">
+      <>
         <Top />
-        <About />
         <Projects />
-      </div>
+      </>
     );
   }
 }
