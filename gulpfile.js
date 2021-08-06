8700a9e2fe6da672184f1357660a461596790814
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2021-08-06 12:13:34 +0300

    Added a gulp task that screenshots the sites on different devices

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const Pageres = require("pageres");
+const path = require("path");
+
+async function screenshotTask() {
+  const sites = [
+    { name: "demacia", url: "https://demacia5635.github.io/" },
+    { name: "school", url: "https://noamalffasy.github.io/SchoolWebsite/" },
+    { name: "oyah", url: "https://oyah-200816.appspot.com" },
+    { name: "hebrew-game", url: "https://noamalffasy.github.io/HebrewWork/" },
+    { name: "elmix", url: "https://elmix-website.herokuapp.com/" },
+  ];
+
+  for (const site of sites) {
+    await new Pageres({
+      filename: `${site.name}-<%= width %>`,
+      crop: true,
+      delay: 15,
+      launchOptions: { args: ["--autoplay-policy=no-user-gesture-required"] },
+    })
+      .src(site.url, [
+        "375x667",
+        "640x565",
+        "768x1024",
+        "1024x768",
+        "1280x720",
+        "1920x1080",
+      ])
+      .dest(path.resolve(__dirname, "public", "img"))
+      .run();
+  }
+}
+
+exports.default = screenshotTask;
