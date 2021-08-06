0e287977560297a1ebad4ee2fb1709fb1278f04b
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2021-08-06 12:12:27 +0300

    Linked to the custom fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,16 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
