46a3ee4c335c53077355992ae42acac78bcc1d1c
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2021-08-08 16:17:30 +0300

    Improved SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,31 @@ import { Component } from "react";
 
 import Top from "components/Top";
 import Projects from "components/Projects";
+import { NextSeo } from "next-seo";
 
 class Index extends Component {
   render() {
     return (
       <>
+        <NextSeo
+          title="Noam Alffasy"
+          description="Full Stack Developer"
+          canonical="https://noamalffasy.vercel.app"
+          openGraph={{
+            locale: "en_US",
+            url: "https://noamalffasy.vercel.app",
+            title: "Noam Alffasy",
+            description: "Full Stack Developer",
+            images: [
+              { url: "https://noamalffasy.vercel.app/img/og-image.png" },
+            ],
+            site_name: "Noam Alffasy",
+          }}
+          twitter={{
+            handle: "@noamalffasy",
+            site: "@noamalffasy",
+          }}
+        />
         <Top />
         <Projects />
       </>
